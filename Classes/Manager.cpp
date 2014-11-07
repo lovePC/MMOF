0741f8f6e6c81919697f9d04830798acedda90a3
lovePC <852575016@qq.com>
2014-11-07 16:42:58 +0800

    test0.2

---
+++ Classes/Manager.cpp
@@ -0,0 +1,27 @@
+#include "Manager.h"
+
+Manager* Manager::m_manager=NULL;
+
+Manager::Manager():lineVector()
+{
+
+}
+
+Manager* Manager::getInstance()
+{
+	if (m_manager==NULL)
+	{
+		m_manager=new Manager();
+	}
+
+	return m_manager;
+}
+
+void Manager::freeInstance(void)
+{
+	if (m_manager != NULL)
+	{
+		delete m_manager;
+		m_manager=NULL;
+	}
+}
---
+++ Classes/Manager.h
@@ -0,0 +1,27 @@
+#ifndef _MANAGER_H_
+#define _MANAGER_H_
+#include "cocos2d.h"
+#include "MirrorManager.h"
+#include "Tool.h"
+#include "Line.h"
+#include "Light.h"
+
+USING_NS_CC;
+//单例
+//用于存放路径，镜子和光
+class Manager
+{
+public:
+	static Manager* getInstance();
+	static void Manager::freeInstance(void);
+private:
+	static Manager* m_manager;
+	Manager();
+	bool init();
+public:
+	Vector<Line> lineVector;
+	//Vector<light*> lightVector;
+	//Vector<Mirror*> mirrorVector;
+};
+
+#endif _MANAGER_H_
